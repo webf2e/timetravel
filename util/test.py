8c392337be14867fa389a713ec2d4c0aa771ff6a
webf2e <webf2e@163.com>
2018-11-23 00:56:12 +0800

    添加画心工具类

--- util/test.py
+++ util/test.py
@@ -2,15 +2,19 @@ from PIL import Image
 
 lines = open("/home/liuwenbin/Desktop/location","r")
 im = Image.open("/home/liuwenbin/Desktop/w.jpg")
+img1 = Image.open("/home/liuwenbin/Desktop/gallery-img1.jpg")
+im.resize((2000,1500)).save('/home/liuwenbin/Desktop/w.jpg')
+im = Image.open("/home/liuwenbin/Desktop/w.jpg")
 for line in lines:
     line = line.strip()
     if line == "":
         continue
     line = line.replace("\t",",")
     vals = line.split(",")
-    t = (int(vals[0]),int(vals[1]),int(vals[2]) + int(vals[0]),int(vals[3]) + int(vals[1]))
+    t = (int(vals[0])*2,int(vals[1])*2,int(vals[2])*2 + int(vals[0])*2,int(vals[3])*2 + int(vals[1])*2)
     print(t)
-    im.paste("red", t)
+    img1 = img1.resize((int(vals[2])*2, int(vals[3])*2))
+    im.paste(img1, t)
 im.show()
 
 

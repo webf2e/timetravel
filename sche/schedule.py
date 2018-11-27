aaab7416a65c84a0beea3fe71724edfcce60ef3e
liuwb <webf2e@163.com>
2018-11-27 12:07:57 +0800

    修改内外网流量统计方式

--- sche/schedule.py
+++ sche/schedule.py
@@ -2,6 +2,7 @@ from util import FileUtil
 from util.Global import gloVar
 import psutil
 import datetime
+import time
 
 def moveChatFileJob():
     print("转移聊天记录文件开始")
@@ -13,20 +14,28 @@ def makeBigHeartJob():
 
 def systemTongjiJob():
     #获取时间
-    time = datetime.datetime.strftime(datetime.datetime.now(),"%Y-%m-%d %H:%M:%S")
+    t = datetime.datetime.strftime(datetime.datetime.now(),"%Y-%m-%d %H:%M:%S")
     hour = datetime.datetime.strftime(datetime.datetime.now(), "%Y-%m-%d-%H")
     memory = psutil.virtual_memory().percent
-    FileUtil.writeSystemTongji("memory",hour,"{}\t{}".format(time,memory))
+    FileUtil.writeSystemTongji("memory",hour,"{}\t{}".format(t,memory))
     disk = psutil.disk_usage("/").percent
-    FileUtil.writeSystemTongji("disk", hour, "{}\t{}".format(time, disk))
+    FileUtil.writeSystemTongji("disk", hour, "{}\t{}".format(t, disk))
     cpu = psutil.cpu_percent(0)
-    FileUtil.writeSystemTongji("cpu", hour, "{}\t{}".format(time, cpu))
+    FileUtil.writeSystemTongji("cpu", hour, "{}\t{}".format(t, cpu))
+
+    lastData = {}
     key_info = psutil.net_io_counters(pernic=True).keys()
     for key in key_info:
         if str(key).startswith("eth"):
             recv = psutil.net_io_counters(pernic=True).get(key).bytes_recv
             sent = psutil.net_io_counters(pernic=True).get(key).bytes_sent
-            FileUtil.writeSystemTongji("{}-{}".format("net",key), hour, "{}\t{}\t{}".format(time, recv, sent))
+            lastData[key] = (recv,sent)
+    time.sleep(1)
+    for key in key_info:
+        if str(key).startswith("eth"):
+            recv = psutil.net_io_counters(pernic=True).get(key).bytes_recv - lastData[key][0]
+            sent = psutil.net_io_counters(pernic=True).get(key).bytes_sent - lastData[key][1]
+            FileUtil.writeSystemTongji("{}-{}".format("net",key), hour, "{}\t{}\t{}".format(t, recv, sent))
 
 def removeSystemFileJob():
     FileUtil.removeSystemTongjiFile(240)

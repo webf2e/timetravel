b8fd286a2c35a43b9e64aaa39d726ddcb75c04ad
liuwb <webf2e@163.com>
2018-12-06 17:23:36 +0800

    添加鹰眼相关方法

--- util/YingYanUtil.py
+++ util/YingYanUtil.py
@@ -1,15 +1,19 @@
 import requests
 import json
 
+service_id = 207270
+entity_name = "你的小可爱"
+ak = "fd2PMcjfqa7hRP0xoqGta5w2IODQ2kig"
+
 def addPoint(jsonData):
     url = "http://yingyan.baidu.com/api/v3/track/addpoint"
     data = {}
-    data["ak"] = "fd2PMcjfqa7hRP0xoqGta5w2IODQ2kig"
-    data["service_id"] = 207270
-    data["entity_name"] = "你的小可爱"
+    data["ak"] = ak
+    data["service_id"] = service_id
+    data["entity_name"] = entity_name
     data["latitude"] = jsonData["lat"]
     data["longitude"] = jsonData["lon"]
-    data["loc_time"] = jsonData["timestramp"]
+    data["loc_time"] = jsonData["timestramp"] / 1000
     data["coord_type_input"] = "bd09ll"
     if "height" in jsonData:
         data["height"] = jsonData["height"]
@@ -41,11 +45,45 @@ def addPoint(jsonData):
     if "errorCode" in jsonData:
         data["errorCode"] = jsonData["errorCode"]
     results = requests.post(url, data=json.loads(str(data).replace("'","\""))).text
-    print(results)
+    return results
 
-jsonData={}
-jsonData["lat"] = 40.07853
-jsonData["lon"] = 116.25201
-jsonData["city"] = "北京"
-jsonData["timestramp"] = 1544009304000
-addPoint(jsonData)
+def getLatestPoint():
+    url = "http://yingyan.baidu.com/api/v3/track/getlatestpoint?service_id={}&entity_name={}&coord_type_output=bd09ll&" \
+          "process_option=need_denoise=1,radius_threshold=100,need_mapmatch=0,transport_mode=auto&ak={}"\
+        .format(service_id,entity_name,ak)
+    results = requests.get(url).text
+    return results
+
+def getDistance(startTime,endTime):
+    url = "http://yingyan.baidu.com/api/v3/track/getdistance?ak={}&service_id={}&entity_name={}&is_processed=1&" \
+          "process_option=need_denoise=1,radius_threshold=100,need_mapmatch=0,transport_mode=auto&" \
+          "supplement_mode=walking&low_speed_threshold=20&start_time={}&end_time={}" \
+        .format(ak, service_id, entity_name, startTime, endTime)
+    results = requests.get(url).text
+    return results
+
+def getTrack(startTime,endTime,pageIndex,pageSize):
+    url = "http://yingyan.baidu.com/api/v3/track/gettrack?ak={}&service_id={}&entity_name={}&is_processed=1&" \
+          "process_option=need_denoise=1,need_vacuate=1,radius_threshold=100,need_mapmatch=0,transport_mode=auto&" \
+          "supplement_mode=walking&low_speed_threshold=20&coord_type_output=bd09ll&sort_type=asc&" \
+          "start_time={}&end_time={}&page_index={}&page_size={}" \
+        .format(ak, service_id, entity_name, startTime, endTime, pageIndex, pageSize)
+    results = requests.get(url).text
+    return results
+
+
+def getStayPoint(startTime,endTime,stayTime,stayRadius):
+    url = "http://yingyan.baidu.com/api/v3/analysis/staypoint?ak={}&service_id={}&entity_name={}&" \
+          "start_time={}&end_time={}&stay_time={}&stay_radius={}&coord_type_output=bd09ll&" \
+          "process_option=need_denoise=1,need_vacuate=1,radius_threshold=100,need_mapmatch=0,transport_mode=auto" \
+        .format(ak, service_id, entity_name, startTime, endTime, stayTime, stayRadius)
+    results = requests.get(url).text
+    return results
+
+# jsonData={}
+# jsonData["lat"] = 40.07853
+# jsonData["lon"] = 116.25201
+# jsonData["city"] = "北京"
+# jsonData["timestramp"] = 1544009304000
+# addPoint(jsonData)
+

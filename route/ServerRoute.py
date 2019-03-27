9af6524dbd83449e6f1d077507806c92428bfa93
liuwb <webf2e@163.com>
2019-03-27 11:46:08 +0800

    添加服务检测接口

--- route/ServerRoute.py
+++ route/ServerRoute.py
@@ -15,3 +15,8 @@ def sshLogin():
     PushUtil.pushToSingle("服务器登录", "{}_{}".format(user, clientIp), "")
     logging.warning("服务器通过ssh登录，登录用户：{}，登录客户端IP：{}，登录时间：{}".format(user, clientIp, time))
     return "OK"
+
+#该接口供server项目调用，重启后，调用该接口判断是否重启成功
+@serverRoute.route('/restart/check',methods=["GET"])
+def restartCheck():
+    return "OK"
